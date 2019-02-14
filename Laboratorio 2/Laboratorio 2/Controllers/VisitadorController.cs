f43ad2a5368033211a97a87433016b9cfdcac7c3
Karenina <karenizabel1999@gmail.com>
2019-02-14 07:54:22 -0600

    Laboratorio2

---
+++ Laboratorio 2/Laboratorio 2/Controllers/VisitadorController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laboratorio_2.Models;
+
+namespace Laboratorio_2.Controllers
+{
+    public class VisitadorController : Controller
+    {
+        // GET: Visitador
+        List<Visitador> Visitadores = new List<Visitador>();
+        static Visitador v1;
+        [HttpPost]
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MostrarEmpleados()
+        {
+            Visitador[] Visi;
+            Visi = Visitadores.ToArray();
+            return View(Visi);
+        }
+        public ActionResult EmpleadosOficina()
+        {
+
+            return View(Visitadores);
+        }
+        public ActionResult EmpleadosTrabajando()
+        {
+            return View(Visitadores);
+        }
+        public ActionResult NuevoEmpleado()
+        {
+            return View();
+        }
+        public ActionResult Ingresa(string Nombre, int Codigo, int HoraLlegada)
+        {
+             v1 = new Visitador { Nombre = Nombre, Codigo = Codigo, HoraLlegada = HoraLlegada };
+            Visitadores.Add(v1);
+            return View();
+        }
+        public ActionResult BUSQUEDA ()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Laboratorio 2/Laboratorio 2/Models/Visitador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio_2.Models
+{
+    
+    public class Visitador
+    {
+        public string Nombre { get; set; }
+        public int Codigo { get; set; }
+        public int HoraLlegada { get; set; }
+        public int NumCitas;
+        public int HoraRegreso;
+        public bool diponible = true;
+        public int CantHoras;
+        public float Sueldo;
+        public Visitador ()
+        {
+            Random num = new Random();
+            NumCitas = num.Next(0,4);
+        }
+
+        
+    }
+   
+}
